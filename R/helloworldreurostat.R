bb4b652c78aa5981cb9344aca884a62ae0859bb7
Albrecht Gradmann <agradmann@gmail.com>
2014-07-07 14:54:36 +0200

    Adding dummy function, NAMESPACE

---
+++ R/helloworldreurostat.R
@@ -0,0 +1,11 @@
+#' Print hello
+#'
+#' This function prints hello 
+#'
+#' @param fname First name
+#' @param lname Last name
+#' @export
+#' @examples
+#' helloreurostat(fname="Albrecht",lname="Gradmann")
+#' helloreurostat(fname="Stefan",lname="Luedtke")
+helloreurostat <- function(fname, lname){cat(paste(fname,lname))}

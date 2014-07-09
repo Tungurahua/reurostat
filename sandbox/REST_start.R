8017bfe8bc7f659dccaa10cce23d6876812f4812
Albrecht Gradmann <agradmann@gmail.com>
2014-07-09 22:08:11 +0200

    Playing around with REST again

--- sandbox/REST_start.R
+++ sandbox/REST_start.R
@@ -1,18 +1,37 @@
 library(RCurl)
 library(XML)
 
-# This is the URL taken from the EUROSTAT REST Api Description
-u = "http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/ei_naag_q/Q..TOTAL.NA-B1G-CP-MIO-EUR.RO+DE/?startperiod=2012&endPeriod=2013" 
-  
+# This script implements the example REST calls from the Eurostat website
+#
+# http://epp.eurostat.ec.europa.eu/portal/page/portal/sdmx_web_services/getting_started/rest_sdmx_2.1#ind_20_1
+#
+# !! If a HTML redirect message is returned, you probably need to strip the 
+# "www" from the beginning of the URL
 
-# We need to specify the User-Agent, otherwise the request will be denied
-db = getURI(u, httpheader=list('User-Agent'='R')) 
+## Dataflow of available datasets
+# URL to download
+u1 <- "http://ec.europa.eu/eurostat/SDMX/diss-web/rest/dataflow/ESTAT/all/latest" 
 
-# Parse the result as xml
-data <-  xmlParse(db,asText=T)
+doc1 <- getURL(u1,httpheader=list('User-Agent'='R'))
 
-#convert xml to list
-xml_data <- xmlToList(doc)
+docxml1 <- xmlParse(doc1)
+docxml1
+
+## Datastructure for a dataset structure definition (DSD)
+u2 <- "http://ec.europa.eu/eurostat/SDMX/diss-web/rest/datastructure/ESTAT/DSD_nama_gdp_c" 
+
+doc2 <- getURL(u2,httpheader=list('User-Agent'='R'))
+docxml2 <- xmlParse(doc2)
+docxml2
+
+## retrieving data
+u3 <- "http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/cdh_e_fos/..PC.FOS1.BE/?startperiod=2005&endPeriod=2011" 
+
+doc3 <- getURL(u3,httpheader=list('User-Agent'='R'))
+docxml3 <- xmlParse(doc3)
+docxml3
+
+
 
 ##http://technistas.com/2012/06/16/using-rest-apis-from-r-xml-operations/
-campaignDOM = xmlRoot(xmlTreeParse(db))
+#campaignDOM = xmlRoot(xmlTreeParse(db))

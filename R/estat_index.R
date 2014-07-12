e8d7ce7c3924a86fc9dea21dd69be99e7d90245e
Albrecht Gradmann <agradmann@gmail.com>
2014-07-13 01:14:44 +0200

    added local version of index list

--- R/estat_index.R
+++ R/estat_index.R
@@ -4,20 +4,36 @@
 #' using the websites REST service.
 #'
 #' @param lang language used to display name of dataflow. \code{en} for english (default),
-#' \code{de} for german or \code{fr} for french. 
-#' @param url address of the dataflow list (REST) 
+#' \code{de} for german or \code{fr} for french.
+#' @param online Boolean to indicate if the data should be returned from a 
+#' file included in this package (\code{FALSE}) or if it should be downloaded from 
+#' Eurostat directly (\code{FALSE}). The local file will load faster but might 
+#' be outdated). 
+#' @param url address of the dataflow list (REST). This could be used when Eurostat
+#' changes the respective address of the REST service
 #' @import RCurl XML
 #' @export
 #' @return dataframe with two variables ID and name
 estat_index <- function(lang = c("en","de","fr"), 
+                        online = FALSE,
                         url = "http://ec.europa.eu/eurostat/SDMX/diss-web/rest/dataflow/ESTAT/all/latest") 
   {
+  # Test Arguments
   lang <- match.arg(lang)
-  ## [TODO] test if url is url indeed
+  if(!is.logical(online)) stop("'online' must be either TRUE or FALSE")
+  ## [TODO] test if url is url indeed a valid url
   
   ## get xml content
+  if(online==FALSE){
+    load(system.file("largedata/dataflowlist.rda", package="reurostat"))
+  }
+    
+  if(online==TRUE){
   doc <- getURL(url,
                 httpheader=list('User-Agent'='R'))
+  }
+  
+  
   
   ## internal tree supposed to be faster according to doc
   xmlfile <- xmlInternalTreeParse(doc) 
